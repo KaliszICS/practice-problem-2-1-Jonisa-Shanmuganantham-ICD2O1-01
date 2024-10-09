1eab715c2d4cc7ee987f5e8f27bb19daac53047a
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-10-09 13:43:59 +0000

    abc

--- PP2_1.py
+++ PP2_1.py
@@ -1,16 +1,27 @@
 
 
 def q1(): 
-  #Write Assignment code here
-
-
+  integer = int(input("Input an integer: "))
+  num = integer % 2
+  if num == 0:
+    print(f"{integer} is even")
+  if num == 1:
+    print(f"{integer} is odd")
+          
 def q2(): 
-  #Write Assignment code here
+  name = input("Input a name: ")
+  if name == "Kalisz":
+    print("teacher")
+  else:
+    print("student")
+    
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
+'''
 q1()
 q2()
+'''
